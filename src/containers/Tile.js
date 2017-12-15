2e178a724aff536fe3426468727e1ab785d38996
Andrew Rogers <andrewprogers@gmail.com>
2017-12-14 20:22:58 -0500

    clean up Tile component

--- src/containers/Tile.js
+++ src/containers/Tile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Bar from '../components/Bar';
 import utils from '../temp/utils';
 
-const BAR_WIDTH = 8
+const BAR_SIZE = 8
 
 class Tile extends React.Component {
   constructor(props) {
@@ -16,15 +16,17 @@ class Tile extends React.Component {
 
   clickHandler(e) {
     e.stopPropagation();
-    let rect = e.currentTarget.getBoundingClientRect();
-    let proportion = {
-      x: (e.clientX - rect.x) / this.props.width,
-      y: (e.clientY - rect.y) / this.props.height
-    }
+    if (this.state.divider === null) {
+      let rect = e.currentTarget.getBoundingClientRect();
+      let proportion = {
+        x: (e.clientX - rect.x) / this.props.width,
+        y: (e.clientY - rect.y) / this.props.height
+      }
 
-    this.setState({
-      divider: proportion.x
-    })
+      this.setState({
+        divider: proportion.x
+      })
+    }
   }
 
   render() {
@@ -36,19 +38,20 @@ class Tile extends React.Component {
 
     let children = null;
     if (this.state.divider !== null) {
-      let firstTileWidth = this.state.divider * this.props.width - BAR_WIDTH / 2;
+      let firstWidth = this.state.divider * this.props.width - BAR_SIZE / 2;
       children = <div>
         <Tile
-          key='tile-1'
           initialColor={utils.randomColor()}
-          width={firstTileWidth}
+          width={firstWidth}
           height={this.props.height}
         />
-      <Bar key={'bar'} width={BAR_WIDTH} height={this.props.height} />
+        <Bar
+          width={BAR_SIZE}
+          height={this.props.height}
+        />
         <Tile
-          key='tile-2'
           initialColor={utils.randomColor()}
-          width={this.props.width - firstTileWidth - BAR_WIDTH}
+          width={this.props.width - firstWidth - BAR_SIZE}
           height={this.props.height}
         />
       </div>
